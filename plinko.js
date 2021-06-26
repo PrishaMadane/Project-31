59de5f263cacdb0936eb88a0101ad52875839775
PrishaMadane <70759166+PrishaMadane@users.noreply.github.com>
2021-06-26 05:24:25 -0200

    Update plinko.js

--- plinko.js
+++ plinko.js
@@ -1,11 +1,12 @@
 class Plinko {
-    constructor(x,y,r){
+    constructor(x,y){
         var options={
-
+           restitution:1,
+            friction:0,
             isStatic:true
      }
+        this.r=10;
         this.body= Bodies.circle(x,y,this.r,options);
-        this.r=r;
         World.add(world,this.body);
 
     }   display(){
